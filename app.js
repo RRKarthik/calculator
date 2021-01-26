81cb67f18e3224621ee6e049d244692ee2f3aa6f
RRKarthik <raviramkarthik7@gmail.com>
2021-01-27 00:00:04 +0530

    Added functionality

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 let value = "";
-function calc(string)
+let a ="";
+let b= "";
+let op = "";
+let flag = 1;
+function calc(a,b,sign)
 {
-   /*switch(sign){
-       case '+': return a + b;
+   switch(sign){
+       case '+': return +a + +b;
                   break;
         case '-': return a - b;
                   break;
@@ -12,12 +16,59 @@ function calc(string)
                    break;
         default: 
                  break;                                         
-   }*/
+   }
    
 }
 
+function getSign(e)
+{
+   if(flag == 0)
+   {
+      
+      a = calc(a,b,op);
+      display.textContent = a;
+      b="";
+   }
+  op = e.target.getAttribute("data-digit");
+
+  flag = 0;
+}
+
+function feeder(e)
+{  
+  let temp = e.target.getAttribute("data-digit") ;
+   
+   if(temp != "=" && flag == 1)
+   {
+      a = a + temp;
+      display.textContent = a;
+   }
+   if(temp != "=" && flag == 0)
+   {
+      b = b + temp;
+      display.textContent = b;
+   }
+   if(temp == "=")
+   {
+      display.textContent = (calc(a,b,op));
+   }
+
+   
+}
+
+const operations = document.querySelectorAll(".operation");
+operations.forEach(operation => operation.addEventListener("click",getSign));
+
+const buttons = document.querySelectorAll(".numbers");
+buttons.forEach((button)=>button.addEventListener("click",feeder));
+
+const display = document.querySelector("p");
 
 
+
+
+
+/*
 function feeder(e)
 {  
   let temp = e.target.getAttribute("data-digit");
@@ -30,10 +81,15 @@ function feeder(e)
    if(temp == "=")
    {
        console.log(value);
+       if(value.includes("+")){
+          display.textContent = (calc(value.split("+")[0],value.split("+")[1],"+"));
+       }else if(value.includes("-")){
+        display.textContent =(calc(value.split("-")[0],value.split("-")[1],"-"));
+     }else if(value.includes("*")){
+        display.textContent =(calc(value.split("*")[0],value.split("*")[1],"*"));
+     }else{
+        display.textContent =(calc(value.split("/")[0],value.split("/")[1],"/"));  
+     }
    }
 }
-
-const buttons = document.querySelectorAll("button");
-buttons.forEach((button)=>button.addEventListener("click",feeder));
-
-const display = document.querySelector("p");
+*/
